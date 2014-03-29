92894ca368160ec815f50b3656cc48fd9a04ee3c
Alberto Fernández <albertofem@gmail.com>
2014-03-29 19:32:05 +0100

    Migrated most of the layout and CSS

--- src/GJA/GameJam/CompoBundle/Controller/FrontendController.php
+++ src/GJA/GameJam/CompoBundle/Controller/FrontendController.php
@@ -9,18 +9,41 @@ namespace GJA\GameJam\CompoBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * @Route("/")
  */
-class FrontendController
+class FrontendController extends Controller
 {
     /**
-     * @Route("/", name="gamejam_frontend")
+     * @Route("/", name="gamejam_compo_frontend")
      * @Template()
      */
     public function indexAction()
     {
         return [];
+    }
+
+    /**
+     * @Route("/normas", name="gamejam_compo_frontend_rules")
+     * @Template()
+     */
+    public function rulesAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/_partial/login", name="gamejam_compo_frontend_partial_login")
+     * @Template("GameJamCompoBundle:Frontend:_login.html.twig")
+     */
+    public function partialLoginAction()
+    {
+        $csrfToken = $this->container->has('form.csrf_provider')
+            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+            : null;
+
+        return ['csrf_token' => $csrfToken];
     }
 }
---
+++ src/GJA/GameJam/CompoBundle/Entity/Sponsor.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Copyright (c) 2014 Certadia, SL
+ * All rights reserved
+ */
+
+namespace GJA\GameJam\CompoBundle\Entity;
+
+class Sponsor
+{
+}
