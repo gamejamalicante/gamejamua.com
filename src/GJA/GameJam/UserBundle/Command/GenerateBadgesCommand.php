b4103ae6e25f3755bc8afa8f46ccee2689a5ad6f
Alberto Fernández <albertofem@gmail.com>
2014-07-10 22:52:52 +0200

    Curl checker

--- src/GJA/GameJam/UserBundle/Command/GenerateBadgesCommand.php
+++ src/GJA/GameJam/UserBundle/Command/GenerateBadgesCommand.php
@@ -142,9 +142,19 @@ class GenerateBadgesCommand extends ContainerAwareCommand
 
     private function remoteImageExists($url)
     {
-        if (!$fp = curl_init($url))
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        // don't download content
+        curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if(curl_exec($ch)!==FALSE)
+        {
+            return true;
+        }
+        else
+        {
             return false;
-
-        return true;
+        }
     }
 } 
