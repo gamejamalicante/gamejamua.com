f72300af5386ba78c744ec9e82ff2f67b960f4d0
Alberto Fernández <albertofem@gmail.com>
2014-10-05 13:05:02 +0200

    Fix deploy

--- app/config/deploy/common.rb
+++ app/config/deploy/common.rb
@@ -4,5 +4,16 @@ set :deploy_to, "/var/www/gamejamua.com"
 set :shared_children,     [web_path + "/uploads", app_path + "/logs", web_path + "/blog/app/uploads"]
 set :writable_dirs,       ["app/cache", "app/logs", "web/media"]
 
+task :clear_rest_env do
+desc 'Clear cache and warmup environment'
+
+    capifony_pretty_print '--> Cleaning rest env'
+
+    run "cd #{release_path} ; php app/console cache:clear --env=rest_prod"
+
+    capifony_puts_ok
+end
+
+after 'symfony:cache:warmup', 'clear_rest_env'
 before 'deploy:create_symlink', 'update_blog'
 after 'deploy:create_symlink', 'update_sitemap'
