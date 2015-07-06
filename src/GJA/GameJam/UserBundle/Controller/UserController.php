00d314ab5daf99949417a6f0701670932319d070
Alberto Fernández <albertofem@gmail.com>
2015-07-06 20:29:32 +0200

    Ticket generator

--- src/GJA/GameJam/UserBundle/Controller/UserController.php
+++ src/GJA/GameJam/UserBundle/Controller/UserController.php
@@ -11,6 +11,11 @@
 
 namespace GJA\GameJam\UserBundle\Controller;
 
+use GJA\GameJam\CompoBundle\Entity\Compo;
+use Gregwar\Image\Image;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use TrivialSense\FrameworkCommon\Controller\AbstractController;
 use GJA\GameJam\UserBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -30,5 +35,59 @@ class UserController extends AbstractController
     public function profileAction(User $user)
     {
         return ['user' => $user];
+    }
+
+    /**
+     * @Route("/{user}/ticket", name="gamejam_user_profile")
+     * @ParamConverter("user", options={"mapping":{"user":"username"}})
+     */
+    public function ticketAction(User $user)
+    {
+        if (!$this->isGrantedRole("ROLE_ADMIN") && $user !== $this->getUser()) {
+            throw new AccessDeniedException;
+        }
+
+        $compo = $this->getRepository('GameJamCompoBundle:Compo')->findRunningCompo();
+
+        if (is_null($compo) || !$user->hasAppliedTo($compo)) {
+            return new JsonResponse();
+        }
+
+        return $this->generateTicket($user, $compo);
+    }
+
+    protected function generateTicket(User $user, Compo $compo)
+    {
+        $ticketFile = __DIR__.'/../Resources/badges/ticket.jpg';
+        $fontIdPrice = __DIR__.'/../Resources/badges/adampro.otf';
+        $fontName = __DIR__.'/../Resources/badges/pacifico.ttf';
+
+        $application = $compo->getApplicationForUser($user);
+
+        $image = new Image($ticketFile);
+        $image->useFallback(false);
+
+        $this->drawUsername($image, $user, $fontName);
+        $this->drawPrice($image, $user, $fontIdPrice, $application->getOrder()->getAmount());
+
+        $imageData = $image->get('jpg', 100);
+
+        return new Response(
+            $imageData,
+            200,
+            array(
+                'Content-Type' => 'image/jpeg'
+            )
+        );
+    }
+
+    protected function drawUsername(Image $image, User $user, $fontFile)
+    {
+        $image->write($fontFile, '@' . $user->getUsername(), 2520, 390, 40, 0, 0x000000, 'right');
+    }
+
+    protected function drawPrice(Image $image, User $user, $fontFile, $price)
+    {
+        $image->write($fontFile, $price, 2290, 488, 60, 0, 0x000000, 'right');
     }
 }
